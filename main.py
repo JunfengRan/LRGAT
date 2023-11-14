feat_fix: new original llama and fix false gradient

main.py
@@ -16,7 +16,7 @@ from accelerate import Accelerator
 from torcheval.metrics import functional as FUNC
 
 from config import *
-from model_acc import Network
+from model import Network
 
 
 configs = Config()
@@ -32,45 +32,45 @@ torch.backends.cudnn.deterministic = True
 def evaluate_one_batch(batch, model):
     
     # get prediction
-    logits, v_feat_nuclear, v_predictions = model(**batch)
-    
+    logits = model(**batch)
     true = batch['labels'].to('cpu').numpy()
-    v_pred = torch.argmax(v_predictions, dim=-1).to('cpu').numpy()
     pred = torch.argmax(logits, dim=-1).to('cpu').numpy()
-    v_pred, pred, true = int(v_pred), int(pred), int(true)
+    pred, true = int(pred), int(true)
     
-    return v_pred, pred, true
+    # print('testing logits:', logits)
+    # print('testing pred:', pred)
+    # print('testing true:', true)
+    
+    return pred, true
 
 
 # evaluate step
 def evaluate(test_loader, model):
     model.eval()
-    v_pred_list = []
     pred_list = []
     true_list = []
     
     # category matrix for confusion matrix analysis
-    v_category_matrix = torch.zeros(configs.num_labels, configs.num_labels, dtype=torch.int32)
     category_matrix = torch.zeros(configs.num_labels, configs.num_labels, dtype=torch.int32)
     
     for batch in test_loader:
-        v_pred, pred, true = evaluate_one_batch(batch, model)
-        v_category_matrix[true][v_pred] += 1
+        pred, true = evaluate_one_batch(batch, model)
         category_matrix[true][pred] += 1
-        v_pred_list.append(v_pred)
         pred_list.append(pred)
         true_list.append(true)
     
-    v_pred_list = torch.tensor(v_pred_list)
+    if 0 not in pred_list:
+        pred_list.append(0)
+        true_list.append(0)
+    if 1 not in pred_list:
+        pred_list.append(1)
+        true_list.append(1)
+    if 2 not in pred_list:
+        pred_list.append(2)
+        true_list.append(2)
+    
     pred_list = torch.tensor(pred_list)
     true_list = torch.tensor(true_list)
-    
-    v_micro_result = [FUNC.multiclass_f1_score(v_pred_list, true_list, average="micro", num_classes=configs.num_labels), \
-        FUNC.multiclass_recall(v_pred_list, true_list, average="micro", num_classes=configs.num_labels), \
-        FUNC.multiclass_precision(v_pred_list, true_list, average="micro", num_classes=configs.num_labels)]
-    v_macro_result = [FUNC.multiclass_f1_score(v_pred_list, true_list, average="macro", num_classes=configs.num_labels), \
-        FUNC.multiclass_recall(v_pred_list, true_list, average="macro", num_classes=configs.num_labels), \
-        FUNC.multiclass_precision(v_pred_list, true_list, average="macro", num_classes=configs.num_labels)]
     
     micro_result = [FUNC.multiclass_f1_score(pred_list, true_list, average="micro", num_classes=configs.num_labels), \
         FUNC.multiclass_recall(pred_list, true_list, average="micro", num_classes=configs.num_labels), \
@@ -79,7 +79,7 @@ def evaluate(test_loader, model):
         FUNC.multiclass_recall(pred_list, true_list, average="macro", num_classes=configs.num_labels), \
         FUNC.multiclass_precision(pred_list, true_list, average="macro", num_classes=configs.num_labels)]
     
-    return v_micro_result, v_macro_result, micro_result, macro_result, v_category_matrix, category_matrix
+    return micro_result, macro_result, category_matrix
 
 
 # main function
@@ -110,9 +110,6 @@ def main(train_loader, test_loader):
     early_stop_flag = 0
     max_micro_result = None
     max_macro_result = None
-    max_v_micro_result = None
-    max_v_macro_result = None
-    max_v_category_matrix = None
     max_category_matrix = None
 
     for epoch in range(configs.epochs):
@@ -120,10 +117,13 @@ def main(train_loader, test_loader):
             model.train()
             optimizer.zero_grad()
             
-            logits, v_feat_nuclear, v_predictions = model(**batch)
+            logits = model(**batch)
             labels = batch['labels']
             
-            loss = model.compute_loss(logits, v_feat_nuclear, v_predictions, labels)
+            # print('training logits:', logits)
+            # print('training labels:', labels)
+            
+            loss = model.compute_loss(logits, labels)
             accelerator.backward(loss)
 
             if train_step % configs.gradient_accumulation_steps == 0:
@@ -135,19 +135,14 @@ def main(train_loader, test_loader):
                 print('epoch: {}, step: {}, loss: {}'.format(epoch, train_step, loss))
         
         with torch.no_grad():
-            v_micro_result, v_macro_result, micro_result, macro_result, v_category_matrix, category_matrix = evaluate(test_loader, model)
-            print('epoch {} v_micro_result: {}, epoch {} v_max_macro_result: {}'.format(epoch, v_micro_result, epoch, v_macro_result))
+            micro_result, macro_result, category_matrix = evaluate(test_loader, model)
             print('epoch {} micro_result: {}, epoch {} max_macro_result: {}'.format(epoch, micro_result, epoch, macro_result))
-            print('v_category_matrix:\n{}'.format(v_category_matrix))
             print('category_matrix:\n{}'.format(category_matrix))
             
             if max_micro_result is None or micro_result[0] > max_micro_result[0]:
                 early_stop_flag = 1
                 max_micro_result = micro_result
                 
-                max_v_micro_result = v_micro_result
-                max_v_macro_result = v_macro_result
-                max_v_category_matrix = v_category_matrix
                 max_category_matrix = category_matrix
     
                 # state_dict = {'model': model.state_dict(), 'result': max_micro_result}
@@ -162,7 +157,7 @@ def main(train_loader, test_loader):
         if early_stop_flag >= 10:
             break
 
-    return max_micro_result, max_macro_result, max_v_micro_result, max_v_macro_result, max_v_category_matrix, max_category_matrix
+    return max_micro_result, max_macro_result, max_category_matrix
 
 
 if __name__ == '__main__':
@@ -170,9 +165,10 @@ if __name__ == '__main__':
     print(watermark(packages='peft,torch,loralib,transformers,accelerate,datasets'))
 
     # tokenizer
-    tokenizer = LlamaTokenizer.from_pretrained(configs.llama_cache_path)
-    # tokenizer.add_special_tokens({'pad_token': '[PAD]'})
-    tokenizer.pad_token_id = 0
+    access_token = "hf_token"
+    tokenizer = LlamaTokenizer.from_pretrained(configs.llama_cache_path, use_fast=True, use_auth_token=access_token)
+    tokenizer.padding_side = "left"
+    tokenizer.pad_token_id = tokenizer.unk_token_id
     print(tokenizer)
     
     data_files = {
@@ -185,7 +181,7 @@ if __name__ == '__main__':
 
 
     def merge_texts(example):
-        combined_text = '请结合证据判断如下声明是正确的(标签为0),错误的(标签为1),还是无法判断其正误(标签为2):\n'
+        combined_text = '请结合证据判断下述声明是正确的(标签为0),下述声明是错误的(标签为1),或是证据提供的信息不足以进行判断(标签为2):\n'
         combined_text += '声明:' + example['claim']
         
         if configs.label != 'gold_label':
@@ -215,7 +211,7 @@ if __name__ == '__main__':
         # elif example['label'] == 1:
         #     example['prediction'] = combined_text + '结合证据判断,该声明是错误的,标签为' + str(example['label'])
         # elif example['label'] == 2:
-        #     example['prediction'] = combined_text + '结合证据判断,无法判断该声明的正误,标签为' + str(example['label'])
+        #     example['prediction'] = combined_text + '结合证据判断,证据提供的信息不足以进行判断,标签为' + str(example['label'])
         
         return example
 
@@ -234,7 +230,7 @@ if __name__ == '__main__':
 
     # tokenize
     def tokenize_function(example):
-        return tokenizer(example['prediction'], truncation=True, padding='max_length', max_length=configs.max_seq_len)
+        return tokenizer(example['prediction'], truncation=True, padding='max_length', max_length=configs.max_seq_len, return_tensors='pt')
 
 
     dataset = dataset.map(tokenize_function, batched=True)
@@ -250,10 +246,7 @@ if __name__ == '__main__':
     test_loader = DataLoader(dataset=dataset['test'], shuffle=False, batch_size=configs.eval_batch_size, collate_fn=CustomCollator)
     
     # main 
-    max_micro_result, max_macro_result, max_v_micro_result, max_v_macro_result, max_v_category_matrix, max_category_matrix \
-        = main(train_loader, test_loader)
+    max_micro_result, max_macro_result, max_category_matrix = main(train_loader, test_loader)
 
     print('max_micro_result: {}, max_macro_result: {}'.format(max_micro_result, max_macro_result))
-    print('max_v_micro_result: {}, max_v_macro_result: {}'.format(max_v_micro_result, max_v_macro_result))
-    print('max_v_category_matrix:\n{}'.format(max_v_category_matrix))
     print('max_category_matrix:\n{}'.format(max_category_matrix))

model_lr.py
@@ -1,4 +1,3 @@
-from transformers import BertModel
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -8,7 +7,7 @@ from peft import LoraConfig, get_peft_model
 from accelerate import Accelerator
 
 from config import *
-from gnn_layer_acc import *
+from gnn_layer import *
 
 
 configs = Config()
@@ -92,10 +91,10 @@ class Network(nn.Module):
         print(self.model)
         
         # use hook to obtain labels
-        self.feat_hook_nuclear_train = []
-        self.feat_hook_nuclear_eval = []
-        self.feat_hook_label_train = []
-        self.feat_hook_label_eval = []
+        self.feat_hook_nuclear_train = torch.tensor([]).to(device)
+        self.feat_hook_nuclear_eval = torch.tensor([]).to(device)
+        self.feat_hook_label_train = torch.tensor([]).to(device)
+        self.feat_hook_label_eval = torch.tensor([]).to(device)
 
         nuclear_layer = "base_model.model.model.layers.31.self_attn.v_proj.lora_A.default.gnn_layer_stack.v_sentence-level_gat"
         label_layer = "base_model.model.model.layers.31.self_attn.v_proj.lora_A.default.gnn_layer_stack.v_text-level_gat"
@@ -125,12 +124,20 @@ class Network(nn.Module):
         if self.training:
             if self.evaluation_step != 0:
                 self.evaluation_step = 0
+                if self.feat_hook_nuclear_eval.size(0) != 0:
+                    self.feat_hook_nuclear_eval = torch.tensor([]).to(device)
+                if self.feat_hook_label_eval.size(0) != 0:
+                    self.feat_hook_label_eval = torch.tensor([]).to(device)
             v_feat_nuclear = self.feat_hook_nuclear_train[self.training_step]
             v_feat_label = self.feat_hook_label_train[self.training_step]
         
         else:
             if self.training_step != 0:
                 self.training_step = 0
+                if self.feat_hook_nuclear_train.size(0) != 0:
+                    self.feat_hook_nuclear_train = torch.tensor([]).to(device)
+                if self.feat_hook_label_train.size(0) != 0:
+                    self.feat_hook_label_train = torch.tensor([]).to(device)
             v_feat_nuclear = self.feat_hook_nuclear_eval[self.evaluation_step]
             v_feat_label = self.feat_hook_label_eval[self.evaluation_step]
             
@@ -156,6 +163,9 @@ class Network(nn.Module):
         # label loss
         loss3 = self.label_loss(logits.view(-1, self.model.num_labels), labels.view(-1))
         
+        # loss = loss3
+        # loss = loss1 + loss3
+        # loss = loss2 + loss3
         loss = loss1 + loss2 + loss3
         
         return loss
@@ -163,18 +173,30 @@ class Network(nn.Module):
     
     def hook_nuclear(self, module, feat_in, feat_out):
         if module.training:
-            self.feat_hook_nuclear_train.append(feat_out.data)
+            if self.feat_hook_nuclear_train.size(0) == 0:
+                self.feat_hook_nuclear_train = feat_out.unsqueeze(0)
+            else:
+                self.feat_hook_nuclear_train = torch.cat((self.feat_hook_nuclear_train, feat_out.unsqueeze(0)), dim=0)
         else:
-            self.feat_hook_nuclear_eval.append(feat_out.data)
+            if self.feat_hook_nuclear_eval.size(0) == 0:
+                self.feat_hook_nuclear_eval = feat_out.unsqueeze(0)
+            else:
+                self.feat_hook_nuclear_eval = torch.cat((self.feat_hook_nuclear_eval, feat_out.unsqueeze(0)), dim=0)
         
         return None
 
 
     def hook_label(self, module, feat_in, feat_out):
         if module.training:
-            self.feat_hook_label_train.append(feat_out.data)
+            if self.feat_hook_label_train.size(0) == 0:
+                self.feat_hook_label_train = feat_out.unsqueeze(0)
+            else:
+                self.feat_hook_label_train = torch.cat((self.feat_hook_label_train, feat_out.unsqueeze(0)), dim=0)
         else:
-            self.feat_hook_label_eval.append(feat_out.data)
+            if self.feat_hook_label_eval.size(0) == 0:
+                self.feat_hook_label_eval = feat_out.unsqueeze(0)
+            else:
+                self.feat_hook_label_eval = torch.cat((self.feat_hook_label_eval, feat_out.unsqueeze(0)), dim=0)
         
         return None
     